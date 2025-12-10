9ff3bb6f538fc7f1d66d9d97b8fc45132b662ab8
Juseph Ortiz <acmnhc@gmail.com>
2025-12-10 09:07:59 -0500

    feat: Implement core application pages, tabs navigation, and feed service with updated dependencies.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyCkdlpYbix-Fah5d0XNH-Y5LSnocoORt3c',
+    authDomain: 'spotle-28007.firebaseapp.com',
+    projectId: 'spotle-28007',
+    storageBucket: 'spotle-28007.firebasestorage.app',
+    messagingSenderId: '248447900450',
+    appId: '1:248447900450:web:e73faef6c716c3a993637b',
+    measurementId: 'G-CJYN88MZCY',
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyCkdlpYbix-Fah5d0XNH-Y5LSnocoORt3c',
+    authDomain: 'spotle-28007.firebaseapp.com',
+    projectId: 'spotle-28007',
+    storageBucket: 'spotle-28007.firebasestorage.app',
+    messagingSenderId: '248447900450',
+    appId: '1:248447900450:web:e73faef6c716c3a993637b',
+    measurementId: 'G-CJYN88MZCY',
+  },
+  Public_auth_name: 'project-248447900450',
 };
 
 /*
